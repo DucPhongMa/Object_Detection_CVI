20a1a6a5571134fdc9b563a356b91fc7d0c9d416
maducphong7@gmail.com <maducphong1998>
2023-11-30 01:31:04 -0500

    Fixed the size of predictions

--- project_main.py
+++ project_main.py
@@ -6,7 +6,7 @@ rf = Roboflow(api_key="TCBsNOYUEBBmbox9rfUM")
 project = rf.workspace().project("head-count-ko384")
 model = project.version(1).model
 
-result = model.predict("./pic-5.jpg", confidence=40, overlap=30).json()
+result = model.predict("./pic-1.jpg", confidence=40, overlap=30).json()
 
 labels = [item["class"] for item in result["predictions"]]
 
@@ -21,18 +21,23 @@ print(len(detections))
 label_annotator = sv.LabelAnnotator()
 bounding_box_annotator = sv.BoxAnnotator()
 
-image = cv2.imread("./pic-5.jpg")
+image = cv2.imread("./pic-1.jpg")
+image_height, image_width, _ = image.shape
+
+# Calculate text size and margin-top as a percentage of the image size
+font_scale = int(0.02 * image_width)  # Adjust the multiplier as needed
+margin_top_percentage = 0.05  # Adjust as needed
+margin_top = int(margin_top_percentage * image_height)
 
 annotated_image = bounding_box_annotator.annotate(
     scene=image, detections=detections)
 annotated_image = label_annotator.annotate(
     scene=annotated_image, detections=detections, labels=labels)
 
-# Add the number of predictions as bold text on the image with larger font size (2), red color, and a margin-top of 50 pixels
+# Add the number of predictions as bold text on the image with dynamically calculated font size, red color, and margin-top
 font = cv2.FONT_HERSHEY_SIMPLEX
-margin_top = 50
-thickness = 3  # Set thickness for bold effect
+thickness = 2  # Adjust as needed
 
-cv2.putText(annotated_image, f'Predictions: {len(detections)}', (10, 30 + margin_top), font, 2, (0, 0, 255), thickness, cv2.LINE_AA)
+cv2.putText(annotated_image, f'Predictions: {len(detections)}', (10, font_scale + margin_top), font, font_scale / 20, (0, 0, 255), thickness, cv2.LINE_AA)
 
 sv.plot_image(image=annotated_image, size=(16, 16))
