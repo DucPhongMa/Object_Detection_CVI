ce213d9bf7cb6bdbdebddd6db13a5d9f9ca70bb9
maducphong7@gmail.com <maducphong1998>
2023-12-04 03:24:52 -0500

    update project

--- project_main.py
+++ project_main.py
@@ -1,12 +1,16 @@
 from roboflow import Roboflow
 import supervision as sv
+import matplotlib.pyplot as plt
 import cv2
 
 rf = Roboflow(api_key="TCBsNOYUEBBmbox9rfUM")
 project = rf.workspace().project("head-count-ko384")
-model = project.version(1).model
+model = project.version(2).model
 
-result = model.predict("./pic-1.jpg", confidence=40, overlap=30).json()
+# Load the original image
+original_image = cv2.imread("./pic-6.jpg")
+
+result = model.predict("./pic-6.jpg", confidence=40, overlap=30).json()
 
 labels = [item["class"] for item in result["predictions"]]
 
@@ -14,19 +18,17 @@ detections = sv.Detections.from_roboflow(result)
 
 print(len(detections))
 
-# filter by class
 detections = detections[detections.class_id == 0]
 print(len(detections))
 
 label_annotator = sv.LabelAnnotator()
 bounding_box_annotator = sv.BoxAnnotator()
 
-image = cv2.imread("./pic-1.jpg")
+image = cv2.imread("./pic-6.jpg")
 image_height, image_width, _ = image.shape
 
-# Calculate text size and margin-top as a percentage of the image size
-font_scale = int(0.02 * image_width)  # Adjust the multiplier as needed
-margin_top_percentage = 0.05  # Adjust as needed
+font_scale = int(0.02 * image_width) 
+margin_top_percentage = 0.05 
 margin_top = int(margin_top_percentage * image_height)
 
 annotated_image = bounding_box_annotator.annotate(
@@ -34,10 +36,18 @@ annotated_image = bounding_box_annotator.annotate(
 annotated_image = label_annotator.annotate(
     scene=annotated_image, detections=detections, labels=labels)
 
-# Add the number of predictions as bold text on the image with dynamically calculated font size, red color, and margin-top
 font = cv2.FONT_HERSHEY_SIMPLEX
-thickness = 2  # Adjust as needed
+thickness = 2 
 
-cv2.putText(annotated_image, f'Predictions: {len(detections)}', (10, font_scale + margin_top), font, font_scale / 20, (0, 0, 255), thickness, cv2.LINE_AA)
+cv2.putText(annotated_image, f'Predictions: {len(detections)}', (10, font_scale + margin_top), font, font_scale / 20, (0, 0, 255), 
+            thickness, cv2.LINE_AA)
 
-sv.plot_image(image=annotated_image, size=(16, 16))
+plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+plt.title("Original Image")
+plt.axis("off")
+plt.show()
+
+plt.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB))
+plt.title("Annotated Image")
+plt.axis("off")
+plt.show()
--- project_main_2.py
+++ project_main_2.py
@@ -5,40 +5,34 @@ import matplotlib.pyplot as plt
 
 rf = Roboflow(api_key="TCBsNOYUEBBmbox9rfUM")
 project = rf.workspace().project("head-count-ko384")
-model = project.version(1).model
+model = project.version(2).model
 
-# Open a connection to the webcam (usually 0 for built-in webcams)
 cap = cv2.VideoCapture(0)
 
-# Initialize count variable
+
 people_count = 0
 
 while True:
-    # Capture frame-by-frame
+    
     ret, frame = cap.read()
 
-    # Break the loop if the video capture fails
     if not ret:
         print("Failed to capture frame")
         break
 
-    # Perform object detection on the frame
     result = model.predict(frame, confidence=40, overlap=30).json()
     labels = [item["class"] for item in result["predictions"]]
     detections = sv.Detections.from_roboflow(result)
-    detections = detections[detections.class_id == 0]  # Filter by class (assuming people have class_id 0)
+    detections = detections[detections.class_id == 0]
 
     label_annotator = sv.LabelAnnotator()
     bounding_box_annotator = sv.BoxAnnotator()
 
-    # Annotate the frame with bounding boxes and labels
     annotated_frame = bounding_box_annotator.annotate(scene=frame, detections=detections)
     annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
 
-    # Update people count
     people_count = len(detections)
 
-    # Add the number of people as text on the frame
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = int(0.02 * frame.shape[1])
     margin_top_percentage = 0.05
@@ -48,14 +42,11 @@ while True:
     cv2.putText(annotated_frame, f'People Count: {people_count}', (10, font_scale + margin_top),
                 font, font_scale / 20, (0, 0, 255), thickness, cv2.LINE_AA)
 
-    # Display the annotated frame using matplotlib
     plt.imshow(cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB))
     plt.show()
 
-    # Break the loop if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
